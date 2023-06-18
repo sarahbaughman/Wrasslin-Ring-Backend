42488ce75fa6898711112697e61b67e5639600cf
Sarah Baughman <sarahbaughman92@gmail.com>
2023-06-18 10:38:35 -0400

    installed bcrypt, deleted stuff that I tried on config for past problem

--- server/config.py
+++ server/config.py
@@ -36,21 +36,3 @@ api = Api(app)
 CORS(app)
 
 
-
-
-# TRIED:
-
-# convention = {
-#     "ix": 'ix_%(column_0_label)s',
-#     "uq": "uq_%(table_name)s_%(column_0_name)s",
-#     "ck": "ck_%(table_name)s_%(constraint_name)s",
-#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-#     "pk": "pk_%(table_name)s"
-# }
-
-# metadata = MetaData(naming_convention=convention)
-# db = SQLAlchemy(app, metadata=metadata)
-
-
-
-# migrate = Migrate(app, db, render_as_batch=True)
